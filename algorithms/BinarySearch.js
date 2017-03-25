c8996d0a4101fc6f8bc7e5105d5bd9baf6ce5533
neptuno <ing.ramon.alejandro@gmail.com>
2017-03-24 23:00:32 -0400

    add recursive version binary search

--- algorithms/BinarySearch.js
+++ algorithms/BinarySearch.js
@@ -16,3 +16,24 @@ function binarySearch(array, x){
 }
 
 console.log(binarySearch([2,3,6,10,11], 11));
+
+// Recursive version
+function binarySearch(target, array) {
+  var lo = 0;
+  var hi = array.length - 1;
+  var mid = Math.ceil((hi - lo) / 2);
+  var result = -1;
+  if (target === array[mid]) {
+    return mid;
+  } else if(target > array[mid]) {
+    result = binarySearch(target, array.slice(mid));
+    if (result > -1 ) {
+      result += mid ;
+    }
+  } else {
+    result = binarySearch(target, array.slice(0, mid));
+  }
+
+  return result;
+}
+console.log(binarySearch(6, [2,3,4,5,6,7,8,9]));
